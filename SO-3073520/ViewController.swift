5a6c7ea629f5393d846a4b92df312c95737d98f2
Xavier Schott <xschott@gmail.com>
2018-07-20 16:09:07 -0700

    Xcode 9.4.1 iOS 11.4

--- SO-3073520/ViewController.swift
+++ SO-3073520/ViewController.swift
@@ -1,7 +1,7 @@
 //  ViewController.swift
 //  SO-3073520
 //
-//  Copyright © 2017 Xavier Schott
+//  Copyright © 2017, 2018 Xavier Schott
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
 //  of this software and associated documentation files (the "Software"), to deal
